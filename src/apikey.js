85b77b632af4d28a6e9d0620b96b4758b2c6e023
SusiShushii <140683853+SusiShushii@users.noreply.github.com>
2024-03-04 17:42:10 +0700

    Update apikey.js

--- src/apikey.js
+++ src/apikey.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-    key: "e9262430563c33cb4b9e313bada0e8ec",
+    key: "e9262430563c33cb4b9e313bada0e8eaxc",
     base: "https://api.openweathermap.org/data/2.5/",
   };
